b02b6ca00f012400a2572dc542f0dc3bf38cbc98
LeoLiu-repo <zxc39684@gmail.com>
2026-06-04 15:20:04 +0800

    insert report and involved_mem example

--- database/schema.sql
+++ database/schema.sql
@@ -12,8 +12,8 @@ create table if not exists Member (
     password_hash VARCHAR(255) NOT NULL COMMENT '加密後的登入密碼',
     PRIMARY KEY (memID),
     CONSTRAINT chk_mem_clealv_format check (clearance_lv in ('0', '1', '2', '3')),
-    CONSTRAINT chk_mem_perm_format check (permission in ('D', 'C', 'B', 'A', 'O5')),
-    CONSTRAINT chk_mem_status_format check (mem_status in('normal', 'abnormal', 'treating','dead')) -- 新增dead
+    CONSTRAINT chk_mem_perm_format check (permission in ('D', 'C', 'B', 'A','O5')), --待確認O5?
+    CONSTRAINT chk_mem_status_format check (mem_status in('normal', 'abnormal', 'treating','dead')) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- SCP資料
@@ -21,13 +21,13 @@ create table if not exists SCP (
     scpID VARCHAR(10) COMMENT 'SCP代號',
     scp_status VARCHAR(20) COMMENT 'SCP的當前狀態',
     threat_level VARCHAR(10) COMMENT '威脅等級',
-    clearance_lv CHAR(1) DEFAULT '0' COMMENT '安保等級', -- 新增安保等級
+    clearance_lv CHAR(1) DEFAULT '0' COMMENT '安保等級', 
     appearance TEXT COMMENT '外型描述', 
     abilities TEXT COMMENT '特殊能力', 
     weakness TEXT COMMENT '弱點', 
     others TEXT COMMENT '其他', 
     PRIMARY KEY (scpID),
-    CONSTRAINT chk_scp_clealv_format check (clearance_lv in ('0', '1', '2', '3')), -- 新增安保等級
+    CONSTRAINT chk_scp_clealv_format check (clearance_lv in ('0', '1', '2', '3')), 
     CONSTRAINT chk_scp_status_format check (scp_status in ('contained', 'free', 'breached', 'uncontained')),
     CONSTRAINT chk_scp_threat_format check (threat_level in ('Safe', 'Euclid', 'Keter'))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -37,11 +37,11 @@ create table if not exists Mission (
     misID VARCHAR(20) COMMENT '任務代號',
     mis_status VARCHAR(20) COMMENT '任務狀態',
     memID VARCHAR(10) not null COMMENT '成員代號',
-    scpID VARCHAR(5) COMMENT 'SCP代號',
+    scpID VARCHAR(10) not null COMMENT 'SCP代號',
     PRIMARY KEY (misID),
     FOREIGN KEY (memID) REFERENCES Member (memID) ON DELETE RESTRICT,
     FOREIGN KEY (scpID) REFERENCES SCP (scpID) ON DELETE RESTRICT,
-    CONSTRAINT chk_mis_status_format check (mis_status in ('processing', 'completed', 'failed', 'pending'))
+    CONSTRAINT chk_mis_status_format check (mis_status in ('processing', 'completed', 'failed','pending')) --待確認Pending?
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Site (收容措施) 實體表
@@ -63,20 +63,20 @@ CREATE TABLE if not exists Report (
     abilities TEXT COMMENT '特殊能力', 
     weakness TEXT COMMENT '弱點', 
     others TEXT COMMENT '其他', 
-    scpID VARCHAR(5) NOT NULL COMMENT 'SCP代號', 
+    scpID VARCHAR(10) NOT NULL COMMENT 'SCP代號', 
     PRIMARY KEY (reportID),
     FOREIGN KEY (scpID) REFERENCES SCP(scpID) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --------------------------------
 
--- research_leader (研究關係表) 
-CREATE TABLE if not exists research_leader (
+-- involved_mem (研究關係表) 
+CREATE TABLE if not exists involved_mem (
     reportID DATETIME NOT NULL COMMENT '事件編號', 
     memID VARCHAR(10) NOT NULL COMMENT '成員代號', 
     role VARCHAR(20) NOT NULL COMMENT '身分角色', 
     PRIMARY KEY (reportID, memID),
-    CONSTRAINT chk_member_role CHECK (role IN ('leader', 'involved_member')), -- 成員所扮演角色 負責人或涉及成員 負責人一位待確認?
+    CONSTRAINT chk_member_role CHECK (role IN ('leader', 'involved_member')), 
     FOREIGN KEY (reportID) REFERENCES Report(reportID) ON DELETE RESTRICT, 
     FOREIGN KEY (memID) REFERENCES Member(memID) ON DELETE RESTRICT 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -115,7 +115,7 @@ BEGIN
         SET MESSAGE_TEXT = 'Cannot assign a deceased member';
     END IF;
 
-    -- 限制一位負責人：如果新塞入的資料是 leader，去檢查該報告是否已經有負責人 待確認
+    -- 限制一位負責人：如果新塞入的資料是 leader，去檢查該報告是否已經有負責人
     IF NEW.role = 'leader' AND (SELECT COUNT(*) FROM involved_mem WHERE reportID = NEW.reportID AND role = 'leader') > 0 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'leader already exists';
@@ -123,4 +123,3 @@ BEGIN
 
 END //
 delimiter ;
-
